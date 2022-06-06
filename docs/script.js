3f0acdae5f672c0175173e702ba0418823e20949
bambosan <bambosan888@gmail.com>
2022-06-06 13:25:01 +0700

    update docs

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,2 @@
 
-var nuclick = 2;
-navbtn.onclick =()=>{
-    nuclick++;
-    navcon.style.height = nuclick%2 == 1 ? "120px" : "0px";
-    //console.log(nuclick%2);
-}
+navbtn.onclick =()=>{ navcon.classList.toggle("navtog") }
